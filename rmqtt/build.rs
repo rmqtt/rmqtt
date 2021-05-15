4c17dc1a22beda03d95007e6bb64886fad23480e
rmqtt-rs <rmqttd@126.com>
2021-05-16 00:26:17 +0800

    Optimize: code

--- rmqtt/build.rs
+++ rmqtt/build.rs
@@ -1,9 +1,7 @@
 fn main() {
     let out = std::env::var("OUT_DIR").unwrap();
     println!("out: {}", out);
-    let build_res = tonic_build::configure()
-        .out_dir(&format!("{}", out))
-        .compile(&["pb.proto"], &["src/grpc/proto"]);
+    let build_res = tonic_build::configure().out_dir(out).compile(&["pb.proto"], &["src/grpc/proto"]);
     println!("compile proto result! {:?}", build_res);
     build_res.unwrap();
 }
