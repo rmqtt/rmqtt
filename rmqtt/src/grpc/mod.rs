c3f7fa50700f5ec029b4e9d38d06f0c5d95fb278
rmqtt-rs <rmqttd@126.com>
2021-05-23 17:47:52 +0800

    Add: Message::Kick, Message::GetRetains, Message::NumberOfClients, Message::NumberOfSessions, MessageReply::Error, MessageReply::Kick, MessageReply::GetRetains, MessageReply::NumberOfClients, MessageReply::NumberOfSessions

--- rmqtt/src/grpc/mod.rs
+++ rmqtt/src/grpc/mod.rs
@@ -6,20 +6,31 @@ pub(crate) mod pb {
     include!(concat!(env!("OUT_DIR"), "/pb.rs"));
 }
 
-use crate::broker::types::{From, Publish};
+use crate::broker::session::SessionOfflineInfo;
+use crate::broker::types::{From, Id, Publish, Retain, Topic};
 use crate::Result;
+use bytes::Bytes;
+
+///Reserved within 1000
+pub type MessageType = u64;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Message {
     Forward(From, Publish),
     Forwards(Vec<(From, Publish)>),
-    Data(Vec<u8>),
+    Kick(Id),
+    GetRetains(Topic),
+    NumberOfClients,
+    NumberOfSessions,
+    Bytes(Bytes),
 }
 
 impl Message {
+    #[inline]
     pub fn encode(&self) -> Result<Vec<u8>> {
         Ok(bincode::serialize(self).map_err(anyhow::Error::new)?)
     }
+    #[inline]
     pub fn decode(data: &[u8]) -> Result<Message> {
         Ok(bincode::deserialize::<Message>(data).map_err(anyhow::Error::new)?)
     }
@@ -27,14 +38,21 @@ impl Message {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum MessageReply {
-    Data(Vec<u8>),
     Success,
+    Error(String),
+    Kick(Option<SessionOfflineInfo>),
+    GetRetains(Vec<(Topic, Retain)>),
+    NumberOfClients(usize),
+    NumberOfSessions(usize),
+    Bytes(Vec<u8>),
 }
 
 impl MessageReply {
+    #[inline]
     pub fn encode(&self) -> Result<Vec<u8>> {
         Ok(bincode::serialize(self).map_err(anyhow::Error::new)?)
     }
+    #[inline]
     pub fn decode(data: &[u8]) -> Result<MessageReply> {
         Ok(bincode::deserialize::<MessageReply>(data).map_err(anyhow::Error::new)?)
     }
