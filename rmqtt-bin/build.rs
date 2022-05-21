43c0b741686dc5a0f8c017a5d14deaf67a87d841
rmqtt-rs <rmqttd@126.com>
2022-05-21 19:58:49 +0800

    Rename: init(..) => register(..)

--- rmqtt-bin/build.rs
+++ rmqtt-bin/build.rs
@@ -30,7 +30,7 @@ fn plugins(decoded: &toml::Value) {
             );
 
             inits.push(format!(
-                "    {}::init(rmqtt::Runtime::instance(), r#\"{}\"#, r#\"{}\"#, {}).await?;",
+                "    {}::register(rmqtt::Runtime::instance(), r#\"{}\"#, r#\"{}\"#, {}).await?;",
                 plugin_id, name, descr, default_startup
             ));
         }
