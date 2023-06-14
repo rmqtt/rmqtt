a01d2ce60f577fcdd3028b2375623e56165881dc
rmqtt <rmqttd@126.com>
2023-06-14 23:20:08 +0800

    Optimize the Bytesize, impl Serialize for Bytesize, Bytesize::string()

--- rmqtt/src/settings/mod.rs
+++ rmqtt/src/settings/mod.rs
@@ -220,13 +220,47 @@ impl Plugins {
 #[derive(Debug, Clone, Default, Deserialize)]
 pub struct Mqtt {}
 
-#[derive(Debug, Clone)]
+const BYTESIZE_K: usize = 1024;
+const BYTESIZE_M: usize = 1048576;
+const BYTESIZE_G: usize = 1073741824;
+
+#[derive(Clone)]
 pub struct Bytesize(usize);
 
 impl Bytesize {
     #[inline]
     pub fn as_u32(&self) -> u32 {
         self.0 as u32
+    }
+
+    #[inline]
+    pub fn string(&self) -> String {
+        let mut v = self.0;
+        let mut res = String::new();
+
+        let g = v / BYTESIZE_G;
+        if g > 0 {
+            res.push_str(&format!("{}G", g));
+            v %= BYTESIZE_G;
+        }
+
+        let m = v / BYTESIZE_M;
+        if m > 0 {
+            res.push_str(&format!("{}M", m));
+            v %= BYTESIZE_M;
+        }
+
+        let k = v / BYTESIZE_K;
+        if k > 0 {
+            res.push_str(&format!("{}K", k));
+            v %= BYTESIZE_K;
+        }
+
+        if v > 0 {
+            res.push_str(&format!("{}B", v));
+        }
+
+        res
     }
 }
 
@@ -240,6 +274,35 @@ impl Deref for Bytesize {
 impl DerefMut for Bytesize {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+impl From<usize> for Bytesize {
+    fn from(v: usize) -> Self {
+        Bytesize(v)
+    }
+}
+
+impl From<&str> for Bytesize {
+    fn from(v: &str) -> Self {
+        Bytesize(to_bytesize(v))
+    }
+}
+
+impl fmt::Debug for Bytesize {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.string())?;
+        Ok(())
+    }
+}
+
+impl Serialize for Bytesize {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
     }
 }
 
@@ -270,9 +333,9 @@ pub fn to_bytesize(text: &str) -> usize {
             };
             match u {
                 'B' => v,
-                'K' => v * 1024,
-                'M' => v * 1048576,
-                'G' => v * 1073741824,
+                'K' => v * BYTESIZE_K,
+                'M' => v * BYTESIZE_M,
+                'G' => v * BYTESIZE_G,
                 _ => 0,
             }
         })
