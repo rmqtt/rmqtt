4e7544b6991f31db4d6b0c009442ecc7013aaa30
Darix SAMANI SIEWE <samanidarix@gmail.com>
2024-06-08 13:11:17 +0100

    setupe app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LayoutApp from './Layout';
+import Home from './pages/Home';
+import router from './routes/routes';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LayoutApp/>}>
+          <Route index element={<Home />} />
+          {
+            router.map(route => <Route path={route.path} Component={route.component} />)
+          }
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
