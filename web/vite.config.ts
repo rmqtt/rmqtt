ec931f8baff2b61c0b803f42eb84f916fc1fa736
Darix SAMANI SIEWE <samanidarix@gmail.com>
2024-06-08 13:14:25 +0100

    vite configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
+ });
