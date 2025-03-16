966e300ba62e19378e55a8b7fa45dc9bc1e6edee
bittcrafter <bitcrafter@126.com>
2025-03-17 00:20:46 +0800

    Update examples

--- rmqtt/examples/simple.rs
+++ rmqtt/examples/simple.rs
@@ -1,33 +1,16 @@
+use rmqtt::context::ServerContext;
 use rmqtt::net::{Builder, Result};
 use rmqtt::server::MqttServer;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    std::env::set_var("RUST_LOG", "simple=debug,rmqtt=debug,rmqtt_net=debug,rmqtt_codec=debug");
+    std::env::set_var("RUST_LOG", "simple=debug,rmqtt=info,rmqtt_net=info,rmqtt_codec=info");
     env_logger::init();
 
-    MqttServer::new()
+    let scx = ServerContext::new().build().await;
+
+    MqttServer::new(scx)
         .listener(Builder::new().name("external/tcp").laddr(([0, 0, 0, 0], 1883).into()).bind()?.tcp()?)
-        .listener(Builder::new().name("internal/tcp").laddr(([0, 0, 0, 0], 11883).into()).bind()?.tcp()?)
-        .listener(
-            Builder::new()
-                .name("external/tls")
-                .laddr(([0, 0, 0, 0], 8883).into())
-                .tls_key(Some("./rmqtt-bin/rmqtt.key"))
-                .tls_cert(Some("./rmqtt-bin/rmqtt.pem"))
-                .bind()?
-                .tls()?,
-        )
-        .listener(Builder::new().name("external/ws").laddr(([0, 0, 0, 0], 8080).into()).bind()?.ws()?)
-        .listener(
-            Builder::new()
-                .name("external/wss")
-                .laddr(([0, 0, 0, 0], 8443).into())
-                .tls_key(Some("./rmqtt-bin/rmqtt.key"))
-                .tls_cert(Some("./rmqtt-bin/rmqtt.pem"))
-                .bind()?
-                .wss()?,
-        )
         .build()
         .run()
         .await?;
