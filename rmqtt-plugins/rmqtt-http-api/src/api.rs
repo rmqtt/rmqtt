ca0f2120b0049be23331de450a8a6edcf17b3e13
bittcrafter <bitcrafter@126.com>
2025-04-04 21:13:01 +0800

    Fix the HTTP-API plugin routing issue caused by the Salvo upgrade.

--- rmqtt-plugins/rmqtt-http-api/src/api.rs
+++ rmqtt-plugins/rmqtt-http-api/src/api.rs
@@ -59,25 +59,21 @@ impl Handler for BearerValidator {
 }
 
 fn route(scx: ServerContext, cfg: PluginConfigType, token: Option<String>) -> Router {
-    let mut router = Router::with_path("api/v1")
-        // .hoop(affix_state::inject(scx))
-        // .hoop(affix_state::inject(cfg))
-        .hoop(affix_state::inject((scx, cfg)))
-        .hoop(api_logger);
+    let mut router = Router::with_path("api/v1").hoop(affix_state::inject((scx, cfg))).hoop(api_logger);
     if let Some(token) = token {
         router = router.hoop(BearerValidator::new(&token));
     }
     router
         .get(list_apis)
-        .push(Router::with_path("brokers").get(get_brokers).push(Router::with_path("<id>").get(get_brokers)))
-        .push(Router::with_path("nodes").get(get_nodes).push(Router::with_path("<id>").get(get_nodes)))
+        .push(Router::with_path("brokers").get(get_brokers).push(Router::with_path("{id}").get(get_brokers)))
+        .push(Router::with_path("nodes").get(get_nodes).push(Router::with_path("{id}").get(get_nodes)))
         .push(Router::with_path("health/check").get(check_health))
         .push(
             Router::with_path("clients")
                 .push(Router::with_path("offlines").get(search_offlines).delete(kick_offlines))
                 .get(search_clients)
                 .push(
-                    Router::with_path("<clientid>")
+                    Router::with_path("{clientid}")
                         .get(get_client)
                         .delete(kick_client)
                         .push(Router::with_path("online").get(check_online)),
@@ -86,9 +82,9 @@ fn route(scx: ServerContext, cfg: PluginConfigType, token: Option<String>) -> Ro
         .push(
             Router::with_path("subscriptions")
                 .get(query_subscriptions)
-                .push(Router::with_path("<clientid>").get(get_client_subscriptions)),
+                .push(Router::with_path("{clientid}").get(get_client_subscriptions)),
         )
-        .push(Router::with_path("routes").get(get_routes).push(Router::with_path("<topic>").get(get_route)))
+        .push(Router::with_path("routes").get(get_routes).push(Router::with_path("{topic}").get(get_route)))
         .push(
             Router::with_path("mqtt")
                 .push(Router::with_path("publish").post(publish))
@@ -98,18 +94,18 @@ fn route(scx: ServerContext, cfg: PluginConfigType, token: Option<String>) -> Ro
         .push(
             Router::with_path("plugins")
                 .get(all_plugins)
-                .push(Router::with_path("<node>").get(node_plugins))
-                .push(Router::with_path("<node>/<plugin>").get(node_plugin_info))
-                .push(Router::with_path("<node>/<plugin>/config").get(node_plugin_config))
-                .push(Router::with_path("<node>/<plugin>/config/reload").put(node_plugin_config_reload))
-                .push(Router::with_path("<node>/<plugin>/load").put(node_plugin_load))
-                .push(Router::with_path("<node>/<plugin>/unload").put(node_plugin_unload)),
+                .push(Router::with_path("{node}").get(node_plugins))
+                .push(Router::with_path("{node}/{plugin}").get(node_plugin_info))
+                .push(Router::with_path("{node}/{plugin}/config").get(node_plugin_config))
+                .push(Router::with_path("{node}/{plugin}/config/reload").put(node_plugin_config_reload))
+                .push(Router::with_path("{node}/{plugin}/load").put(node_plugin_load))
+                .push(Router::with_path("{node}/{plugin}/unload").put(node_plugin_unload)),
         )
         .push(
             Router::with_path("stats")
                 .get(get_stats)
                 .push(Router::with_path("sum").get(get_stats_sum))
-                .push(Router::with_path("<id>").get(get_stats)),
+                .push(Router::with_path("{id}").get(get_stats)),
         )
         .push(
             Router::with_path("metrics")
@@ -118,10 +114,10 @@ fn route(scx: ServerContext, cfg: PluginConfigType, token: Option<String>) -> Ro
                     Router::with_path("prometheus")
                         .get(get_prometheus_metrics)
                         .push(Router::with_path("sum").get(get_prometheus_metrics_sum))
-                        .push(Router::with_path("<id>").get(get_prometheus_metrics)),
+                        .push(Router::with_path("{id}").get(get_prometheus_metrics)),
                 )
                 .push(Router::with_path("sum").get(get_metrics_sum))
-                .push(Router::with_path("<id>").get(get_metrics)),
+                .push(Router::with_path("{id}").get(get_metrics)),
         )
 }
 
