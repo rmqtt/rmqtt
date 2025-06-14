b0dca39d0c2480378d01bff0e600cad80ff70c51
Darix SAMANI SIEWE <samanidarix@gmail.com>
2025-06-14 16:43:15 +0100

    implement htt api gateway as a reserve proxu for building dashbord

--- http-api-gateway/app.py
+++ http-api-gateway/app.py
@@ -1,51 +1,28 @@
-from fastapi import Request, FastAPI, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
-import requests
+app = FastAPI()
 
+# Target backend server to which you want to forward requests
+BACKEND_URL = "http://rmqtt:6060/api/v1"
 
-app = FastAPI(description="HTTP API Gateway that supports cross-domain functionality.", contact={"email": "samanidarix@gmail.com", "tel": "691439424"},title="RMQTT API GATEWAY")
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
+async def proxy(request: Request, path: str):
+    async with httpx.AsyncClient() as client:
+        # Construct full URL to target
+        url = f"{BACKEND_URL}/{path}"
+        
+        # Forward method, headers, and body
+        forwarded_request = client.build_request(
+            method=request.method,
+            url=url,
+            headers=request.headers.raw,
+            content=await request.body()
+        )
 
+        # Send the request
+        backend_response = await client.send(forwarded_request, stream=True)
 
-prefix_router = APIRouter()
-
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": "Welcome to the RMQTT HTTP API GATEWAY."}
-
-@prefix_router.get("/brokers")
-async def get_brokers():
-    
-    response = requests.get('http://rmqtt:6060/api/v1/brokers')
-    return response.json()
-
-
-
-@prefix_router.get("/nodes")
-def get_nodes():
-
-    response = requests.get("'http://rmqtt:6060/api/v1/nodes")
-    return response.json()
-
-
-@prefix_router.get("/clients")
-async def get_clients():
-    
-    response = requests.get('http://rmqtt:6060/api/v1/clients')
-    return response.json()
-
-
-
-app.include_router(prefix_router, prefix="/api/v1", tags=["BROKERS"])
-    
+        # Return the response to the client
+        return Response(
+            content=await backend_response.aread(),
+            status_code=backend_response.status_code,
+            headers=dict(backend_response.headers)
+        )
