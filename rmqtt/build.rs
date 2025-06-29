bbbaf9a6ad08c9eb100ad2d5fb7776b524055669
bittcrafter <bitcrafter@126.com>
2025-06-29 23:02:04 +0800

    style: modernize string formatting syntax

--- rmqtt/build.rs
+++ rmqtt/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let version = rustc_version::version().unwrap();
     let build_time = chrono::Local::now().format("%Y%m%d%H%M%S").to_string();
-    println!("cargo:rustc-env=RUSTC_VERSION={}", version);
-    println!("cargo:rustc-env=RUSTC_BUILD_TIME={}", build_time);
+    println!("cargo:rustc-env=RUSTC_VERSION={version}");
+    println!("cargo:rustc-env=RUSTC_BUILD_TIME={build_time}");
     println!("cargo:rerun-if-changed=build.rs");
 }
